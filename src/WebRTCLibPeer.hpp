13643f9c0c6ba63065ec2537de08d177de729294
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2019-04-09 19:54:59 +0200

    Fix windows build

--- src/WebRTCLibPeer.hpp
+++ src/WebRTCLibPeer.hpp
@@ -1,13 +1,14 @@
 #ifndef WEBRTC_PEER_H
 #define WEBRTC_PEER_H
 
+#include <Godot.hpp> // Godot.hpp must go first, or windows builds breaks
+
 #include "api/peerconnectioninterface.h" // interface for all things needed from WebRTC
 #include "media/base/mediaengine.h" // needed for CreateModularPeerConnectionFactory
 #include <functional> // std::function
 #include <mutex> // mutex @TODO replace std::mutex with Godot mutex
 
 #include "net/WebRTCPeerNative.hpp"
-#include <Godot.hpp>
 
 namespace godot_webrtc {
 
